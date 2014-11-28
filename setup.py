fae497dd506d033ee757e5e70aeb545cc2ce489f
Devon Meunier <devon.meunier@gmail.com>
2014-11-28 10:49:42 -0500

    Add PyPI metadata.

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup(
     name='thorium_htmlpurifier',
     description='HTML Purifier validation for Thorium',
     packages=['thorium_htmlpurifier'],
+    version='0.1.0',
+    author='Devon Meunier',
+    author_email='devon@eventmobi.com',
+    url='https://github.com/meunierd/thorium_htmlpurifier/',
     install_requires=[
         'thorium',
         'html-purifier3>=2.1.0',
